81cce8466e48617287159f7f45f6a442498e9a5e
Mateusz Remiszewski <wizerunekdlafirm@gmail.com>
2018-08-04 20:05:58 +0200

    form calc problem solved

--- app.js
+++ app.js
@@ -66,137 +66,94 @@ document.addEventListener('DOMContentLoaded', function () {
                 } else {
                     list.style.display = 'block';
                 }
-
-
         });
     }
-// DROPDOWN LIST CHOSEN VALUE PRESENTATION
 
-    var formCalc = document.querySelector('.form_calculator');
-    var chairTitle = document.querySelector('.title');
-    var chairColor = document.querySelector('.color');
-    var chairPattern = document.querySelector('.pattern');
-    var chairImg = document.querySelector('.image_part img');
+var titleList = document.querySelectorAll('section.choice_part.inline > div > div:nth-child(1) > ul li');
+var titleSpan = document.querySelector('.title_span');
+var colorList = document.querySelectorAll(' section.choice_part.inline > div > div:nth-child(2) > ul li');
+var colorSpan = document.querySelector('.color');
+var patternList = document.querySelectorAll('section.choice_part.inline > div > div:nth-child(3) > ul li');
+var patternSpan = document.querySelector('.pattern');
 
+var titleValue = document.querySelector('.title.value');
+var colorValue = document.querySelector('.color.value');
+var patternValue = document.querySelector('.pattern.value');
 
-    //CHAIRS COST VALUES
+var img = document.querySelector('section.image_part.inline > img');
 
-    var clair = 200;
-    var margarita = 250;
-    var selena = 270;
+var sum = document.querySelector('.sum strong');
 
-    var color = 50;
+//CHAIRS COST VALUES
+var chair = '200';
 
-    var skora = 150;
-    var tkanina = 100;
+var color = '50';
 
-    //VALUES DIRECTION
+var pattern = '150';
 
-    var titleValue = document.querySelector('.title.value');
-    var colorValue = document.querySelector('.color.value');
-    var patternValue = document.querySelector('.pattern.value');
+function listElement(list,direction,valueDirection,value) {
 
-
-
-    // TRANSPORT CHECKBOX EVENT
-    var transportCheckbox = document.querySelector('.check-box input');
-    var transportLabel = document.querySelector('.transport');
-    var transportValue = document.querySelector('.transport-value');
+    for (var i=0; i<list.length ; i++){
 
 
-    transportCheckbox.addEventListener('click',function () {
+        list[i].addEventListener('click', function () {
 
-        if (transportCheckbox.checked === true){
+            if(this.innerText === 'Czarny'){
+                img.setAttribute('src', 'images/black_chair.png');
+            }
+            if(this.innerText === 'Czerwony'){
+                img.setAttribute('src', 'images/red_chair.png');
+            }
+            if(this.innerText === 'Pomarańczowy'){
+                img.setAttribute('src', 'images/orange_chair.png');
+            }
 
-            transportLabel.innerText = 'Transport';
-            transportValue.innerText = transportCheckbox.dataset.transportPrice; //INSERT TRANSPORT VALUE
+            direction.innerText = this.innerText;
+            valueDirection.innerText = value;
+            
+            if(this.parentElement.style.display === 'block'){
+                this.parentElement.style.display = 'none';
+            }
 
-        }if (transportCheckbox.checked === false) { //UNCHECKED CLEAR VALUES
+            allSum()
 
-            transportLabel.innerText = '';
-            transportValue.innerText ='';
-        }
-
-    });
-
-
-
-
-    for (var i=0; i < panelLists.length; i++){
-
-
-        var labelLi = panelLists[i].querySelectorAll('li');
-
-         for (var j=0; j < labelLi.length; j++) {
-
-             labelLi[j].addEventListener('click', function () {
-
-                 //INSERT TEXT FROM .THIS ELEMENT
-
-                 this.parentElement.parentElement.firstElementChild.innerText = this.innerText;
-                 this.parentElement.style.display = 'none'; //DISPLAY NONE AFTER OPTION SELECTED
-
-
-                 //TYPE OPTION LIST
-                 if(this.parentElement.parentElement.firstElementChild === formCalc.firstElementChild.firstElementChild){
-                     chairTitle.innerText = this.innerText; //INSERT CHAIR NAME TO FORM
-
-                     //PRODUCT TYPE VALUES
-
-                     if(this.innerText === 'Clair'){
-                         titleValue.innerText = clair
-                     }if(this.innerText === 'Margarita') {
-                         titleValue.innerText = margarita
-                     }if(this.innerText === 'Selena') {
-                         titleValue.innerText = selena
-                     }
-
-                 //COLOR OPTION LIST
-                 }if(this.parentElement.parentElement === formCalc.firstElementChild.nextElementSibling ){
-                     chairColor.innerText = this.innerText; //INSERT CHAIR COLOR TO FORM
-                     colorValue.innerText = color;  //PRODUCT VALUES
-
-
-                     //PRODUCT IMG CHANGE
-
-                     if( this.innerText === 'Czerwony'){
-                         chairImg.setAttribute('src', 'images/red_chair.png');
-                     }
-                     if (this.innerText === 'Czarny'){
-                         chairImg.setAttribute('src', 'images/black_chair.png');
-                     }
-                     if (this.innerText === 'Pomarańczowy'){
-                         chairImg.setAttribute('src', 'images/orange_chair.png');
-                     }
-
-                 //PATTERN OPTION LIST
-                 }if(this.parentElement.parentElement === formCalc.firstElementChild.nextElementSibling.nextElementSibling ){
-                     chairPattern.innerText = this.innerText; //INSERT CHAIR PATTERN TO FORM
-
-                     //PRODUCT PATTERN VALUES
-
-                     if(this.innerText === 'Tkanina'){
-                         patternValue.innerText = tkanina
-                     }if(this.innerText === 'Skóra') {
-                         patternValue.innerText = skora
-                     }
-                 }
-
-
-
-                 // SUM VALUES
-
-                 var sum = document.querySelector('.sum strong');
-
-
-                     sum.innerText = titleValue.innerText * 1 + colorValue.innerText * 1 + patternValue.innerText * 1
-
-
-
-             });
-
-         }
+        })
     }
+}
+
+ // TRANSPORT CHECKBOX EVENT
+var transportCheckbox = document.querySelector('.check-box input');
+var transportLabel = document.querySelector('.transport');
+var transportValue = document.querySelector('.transport-value');
+
+
+    transportCheckbox.addEventListener('change',function () {
+
+    if (transportCheckbox.checked === true){
+
+        transportLabel.innerText = 'Transport';
+        transportValue.innerText = transportCheckbox.dataset.transportPrice; //INSERT TRANSPORT VALUE
+        allSum()
+    }if (transportCheckbox.checked === false) { //UNCHECKED CLEAR VALUES
+
+        transportLabel.innerText = '';
+        transportValue.innerText ='';
+        allSum()
+    }
+
+});
+
+function allSum() {
+        sum.innerText = titleValue.innerText *1 + colorValue.innerText*1 + patternValue.innerText*1 + transportValue.innerText *1
+
+}
+
+listElement(titleList,titleSpan,titleValue,chair);
+
+listElement(colorList,colorSpan,colorValue,color);
+
+listElement(patternList,patternSpan,patternValue,pattern);
+
 
 
 
